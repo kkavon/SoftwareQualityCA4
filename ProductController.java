4774c2d322061876602c35813c278f52d09c64ab
kkavon <61874951+kkavon@users.noreply.github.com>
2023-03-28 18:41:38 +0100

    Add files via upload

--- ProductController.java
+++ ProductController.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ProductController {
@@ -16,7 +18,8 @@ public class ProductController {
     private ProductRepository productRepository;
     
     
-    
+
+
     @GetMapping("/stock")
     public String listProduct(Model model) {
         List<Product> products = productRepository.findAll();
@@ -37,5 +40,25 @@ public class ProductController {
         return "redirect:/products";
     }
 
-    // Other controller methods for handling product requests
+
+    @GetMapping("/products/edit/{id}")
+    public String editProductForm(@PathVariable("id") Long id, Model model) {
+        Product product = productRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid product Id:" + id));
+        model.addAttribute("product", product);
+        return "products_edit";
+    }
+    @PostMapping("/products/update/{id}")
+    public String updateProduct(@PathVariable("id") Long id, @ModelAttribute Product product) {
+        productRepository.save(product);
+        return "redirect:/stock";
+    }
+    
+    
+    
+    @GetMapping("/products/search")
+    public String searchProducts(@RequestParam("searchTerm") String searchTerm, Model model) {
+        List<Product> searchResults = productRepository.search(searchTerm);
+        model.addAttribute("products", searchResults);
+        return "products_display";
+    }
 }
--- ProductRepository.java
+++ ProductRepository.java
@@ -1,8 +1,14 @@
 package net.codejava;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+	 @Query("SELECT p FROM Product p WHERE LOWER(p.title) LIKE LOWER(CONCAT('%', :searchTerm, '%'))")
+	    List<Product> search(@Param("searchTerm") String searchTerm);
 }
